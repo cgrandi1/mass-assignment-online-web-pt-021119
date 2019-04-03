21f61fa47c7709dcff1f476c99c19383f071b708
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-03 03:38:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,10 @@
 class Person
   
   attribute = attr_accessor :name:, :birthday, :hair_color, :eye_color, :height:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+    :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize (attribute)
-    attribute.each {|key, value| self.send(("#{key}="), value)}
-  end
+    def initialize (attribute)
+      attribute.each {|key, value| self.send(("#{key}="), value)}
+    end
+  end 
 end
